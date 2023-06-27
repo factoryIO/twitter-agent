9ddb771cc67c8fd21af195bb8b13af7d1ef4c607
factoryIO <53910907+factoryIO@users.noreply.github.com>
2023-06-27 14:22:29 +0200

    Update twitter_client.py
    AI proposed enhancement (to be tested)

--- src/twitter_client.py
+++ src/twitter_client.py
@@ -1,53 +1,99 @@
-import os
+# Import the required libraries
 import tweepy
-import yaml
-from dotenv import load_dotenv
+import logging
 
-# Load environment variables
-load_dotenv()
+# Import the additional modules
+from twitter_api import TwitterAPI
 
-API_KEY = os.getenv("API_KEY", "")
-API_SECRET_KEY = os.getenv("API_SECRET_KEY", "")
-BEARER_TOKEN = os.getenv("BEARER_TOKEN", "")
+# Define the TwitterClient class
+class TwitterClient:
+  # Initialize the class with the Twitter credentials
+  def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+    self.consumer_key = consumer_key
+    self.consumer_secret = consumer_secret
+    self.access_token = access_token
+    self.access_token_secret = access_token_secret
 
-# Load the access tokens and secrets from the YAML file
-with open('./tokens.yml', 'r') as f:
-    tokens = yaml.safe_load(f)
+    # Create a Tweepy API object using the tweepy module
+    self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+    self.auth.set_access_token(self.access_token, self.access_token_secret)
+    self.api = tweepy.API(self.auth)
 
-def _fetch_v1_api(access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    return api
+    # Create a TwitterAPI object using the twitter_api module
+    self.twitter_api = TwitterAPI(self.consumer_key, self.consumer_secret)
 
-def _fetch_client(access_token, access_token_secret):
-    client = tweepy.Client(
-            bearer_token=BEARER_TOKEN,
-            consumer_key=API_KEY,
-            consumer_secret=API_SECRET_KEY,
-            access_token=access_token,
-            access_token_secret=access_token_secret,
-            wait_on_rate_limit=True,
-        )
+  # Define a method to stream tweets from the Twitter API using a filter based on keywords or users
+  def stream_tweets(self, process_tweet):
+    # Create a stream listener class using the tweepy module
+    class StreamListener(tweepy.StreamListener):
+      # Override the on_status method to process each tweet
+      def on_status(self, tweet):
+        # Call the process_tweet function with the tweet as an argument
+        process_tweet(tweet)
 
-    return client
+      # Override the on_error method to handle errors
+      def on_error(self, status_code):
+        # Log the error code
+        logging.error(f"Error: {status_code}")
 
+        # Return False to stop the stream if rate limit is exceeded
+        if status_code == 420:
+          return False
 
-def fetch_clients() -> list:
-    # Initialize a client for each set of access tokens/secrets
-    client_data = []
-    for token in tokens:
-        client = _fetch_client(token['token'], token['secret'])
-        v1_api = _fetch_v1_api(token['token'], token['secret'])
-        strategy = token['strategy']
-        user_name = token['user_name']
-        agent_id = token['id']
-        client_data.append({
-            "client": client,
-            "v1_api": v1_api,
-            "strategy": strategy,
-            "user_name": user_name,
-            "agent_id": agent_id,
-        })
+    # Create a stream listener object using the stream listener class
+    stream_listener = StreamListener()
 
-    return client_data
+    # Create a stream object using the tweepy module and the stream listener object
+    stream = tweepy.Stream(auth=self.api.auth, listener=stream_listener)
+
+    # Start streaming tweets using a filter based on keywords or users
+    stream.filter(track=["keyword1", "keyword2"], follow=["user1", "user2"])
+
+# Define a method to filter tweets based on some criteria such as sentiment or topic
+  def filter_tweets(self, tweet, filter):
+    # Get the tweet's text and author
+    tweet_text = tweet.text
+    tweet_author = tweet.user.screen_name
+
+    # Get the filter's type and parameters
+    filter_type = filter["type"]
+    filter_params = filter["params"]
+
+    # Filter tweets based on some criteria such as sentiment or topic
+    if filter_type == "sentiment":
+      # Check if the tweet has a sentiment using some ML models (not shown here)
+      sentiment = filter_params["sentiment"]
+      match = analyze_sentiment(tweet_text) == sentiment
+
+    elif filter_type == "topic":
+      # Check if the tweet has a topic using some ML models (not shown here)
+      topic = filter_params["topic"]
+      match = analyze_topic(tweet_text) == topic
+
+    else:
+      raise ValueError("Invalid filter")
+
+    # Return the match result
+    return match
+
+  # Define a method to post a reply using Tweepy or TwitterAPI
+  def post_reply(self, tweet, reply, agent):
+    # Get the tweet's id and author
+    tweet_id = tweet.id
+    tweet_author = tweet.user.screen_name
+
+    # Get the agent's name and credentials
+    agent_name = agent["name"]
+    agent_credentials = agent["credentials"]
+
+    # Post a reply using Tweepy or TwitterAPI
+    if agent_credentials == "Tweepy":
+      # Post a reply using Tweepy
+      self.api.update_status(f"@{tweet_author} {reply}", in_reply_to_status_id=tweet_id)
+
+    elif agent_credentials == "TwitterAPI":
+      # Post a reply using TwitterAPI
+      self.twitter_api.update_status(f"@{tweet_author} {reply}", in_reply_to_status_id=tweet_id)
+
+    else:
+      raise ValueError("Invalid credentials")
