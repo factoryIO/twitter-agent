ea1cf880da894e0523915052ee6c1cf4109fa38c
factoryIO <53910907+factoryIO@users.noreply.github.com>
2023-06-27 14:16:45 +0200

    Update main.py
    AI proposed enhancement (to be tested)

--- src/main.py
+++ src/main.py
@@ -1,130 +1,409 @@
-"""Twitter-Agent Entry Point"""
-
+# Import the required libraries
 import os
+import logging
 import json
-import time
 import weaviate
-import yaml
-import pdb
-import asyncio
-from functools import wraps
+import tweepy
+from weaviate_client import WeaviateClient
+from twitter_client import TwitterClient
 
-import click
+# Import the additional modules
+from dotenv import load_dotenv
+from transformers import pipeline
+from rasa.core.agent import Agent
+from rasa.core.interpreter import RasaNLUInterpreter
+from twitter_api import TwitterAPI
+from google_analytics_api import GoogleAnalyticsAPI
+from twitter_analytics_api import TwitterAnalyticsAPI
+from streamlit import st
+from huggingface_spaces import hf_spaces
+from perspective_api import PerspectiveAPI
+from hatebase_api import HatebaseAPI
+import torch
+import tensorflow as tf
+import ray
+import optuna
 
-from langchain.llms import OpenAI
-from langchain.vectorstores import Weaviate
-from langchain.embeddings.openai import OpenAIEmbeddings
+# Load the configuration variables from a .env file
+load_dotenv()
+WEAVIATE_URL = os.getenv("WEAVIATE_URL")
+WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
+WEAVIATE_API_SECRET = os.getenv("WEAVIATE_API_SECRET")
+TWITTER_CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
+TWITTER_CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
+GOOGLE_ANALYTICS_ID = os.getenv("GOOGLE_ANALYTICS_ID")
+GOOGLE_ANALYTICS_KEY = os.getenv("GOOGLE_ANALYTICS_KEY")
+TWITTER_ANALYTICS_ID = os.getenv("TWITTER_ANALYTICS_ID")
+TWITTER_ANALYTICS_KEY = os.getenv("TWITTER_ANALYTICS_KEY")
+STREAMLIT_TOKEN = os.getenv("STREAMLIT_TOKEN")
+HUGGINGFACE_SPACES_TOKEN = os.getenv("HUGGINGFACE_SPACES_TOKEN")
+PERSPECTIVE_API_KEY = os.getenv("PERSPECTIVE_API_KEY")
+HATEBASE_API_KEY = os.getenv("HATEBASE_API_KEY")
+
+# Initialize the Weaviate and Twitter clients
+weaviate_client = WeaviateClient(WEAVIATE_URL, WEAVIATE_API_KEY, WEAVIATE_API_SECRET)
+twitter_client = TwitterClient(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+# Initialize the additional clients and pipelines
+twitter_api_client = TwitterAPI(TWITTER_API_KEY, TWITTER_API_SECRET)
+google_analytics_client = GoogleAnalyticsAPI(GOOGLE_ANALYTICS_ID, GOOGLE_ANALYTICS_KEY)
+twitter_analytics_client = TwitterAnalyticsAPI(TWITTER_ANALYTICS_ID, TWITTER_ANALYTICS_KEY)
+tweet_generator_pipeline = pipeline("text-generation", model="gpt-2")
+persona_generator_pipeline = pipeline("text-generation", model="gpt-3-davinci")
+style_generator_pipeline = pipeline("style-transfer", model="t5-base")
+dialogue_model_agent = Agent.load("dialogue_model_path", interpreter=RasaNLUInterpreter("nlu_model_path"))
+perspective_api_client = PerspectiveAPI(PERSPECTIVE_API_KEY)
+hatebase_api_client = HatebaseAPI(HATEBASE_API_KEY)
+
+# Create the Weaviate schema if it does not exist
+weaviate_client.create_schema()
+
+# Load the agents from a database or a cloud service such as MongoDB or Firebase
+agents = weaviate_client.load_agents()
+
+# Define a function to create a tweet based on the agent's persona and style using GPT-3 and T5 models
+def create_tweet(agent):
+    # Get the agent's name, persona, and style
+    agent_name = agent["name"]
+    agent_persona = agent["persona"]
+    agent_style = agent["style"]
+
+    # Generate a tweet based on the agent's persona using GPT-3 model
+    tweet_input = f"{agent_name}: {agent_persona}"
+    tweet_output = tweet_generator_pipeline(tweet_input, max_length=280)
+
+    # Transfer the tweet to the agent's style using T5 model
+    style_input = f"Transfer {tweet_output} to {agent_style}"
+    style_output = style_generator_pipeline(style_input)
+
+    # Return the styled tweet
+    return style_output
+
+# Define a function to create a persona for an agent using GPT-3 model
+def create_persona(agent):
+    # Get the agent's name and topic
+    agent_name = agent["name"]
+    agent_topic = agent["topic"]
+
+    # Generate a persona based on the agent's name and topic using GPT-3 model
+    persona_input = f"Create a persona for {agent_name} who is interested in {agent_topic}"
+    persona_output = persona_generator_pipeline(persona_input)
+
+# Return the persona
+    return persona_output
+
+# Define a function to create a style for an agent using T5 model
+def create_style(agent):
+    # Get the agent's name and topic
+    agent_name = agent["name"]
+    agent_topic = agent["topic"]
+
+    # Generate a style based on the agent's name and topic using T5 model
+    style_input = f"Create a style for {agent_name} who is interested in {agent_topic}"
+    style_output = style_generator_pipeline(style_input)
+
+    # Return the style
+    return style_output
+
+# Define a function to post a tweet using Tweepy or TwitterAPI
+def post_tweet(tweet, agent):
+    # Get the agent's name and credentials
+    agent_name = agent["name"]
+    agent_credentials = agent["credentials"]
+
+    # Post the tweet using Tweepy or TwitterAPI
+    if agent_credentials == "Tweepy":
+        twitter_client.post_tweet(tweet)
+    elif agent_credentials == "TwitterAPI":
+        twitter_api_client.post_tweet(tweet)
+    else:
+        raise ValueError("Invalid credentials")
+
+# Define a function to collect and analyze data using Google Analytics and Twitter Analytics
+def collect_and_analyze_data(agent):
+    # Get the agent's name and ID
+    agent_name = agent["name"]
+    agent_id = agent["id"]
+
+    # Collect and analyze data using Google Analytics and Twitter Analytics
+    google_analytics_data = google_analytics_client.get_data(agent_id)
+    twitter_analytics_data = twitter_analytics_client.get_data(agent_id)
+
+    # Return the data
+    return google_analytics_data, twitter_analytics_data
+
+# Define a function to create a dashboard using Streamlit or Hugging Face Spaces
+def create_dashboard(data, agent):
+    # Get the agent's name and token
+    agent_name = agent["name"]
+    agent_token = agent["token"]
+
+    # Create a dashboard using Streamlit or Hugging Face Spaces
+    if agent_token == "Streamlit":
+        st.title(f"Dashboard for {agent_name}")
+        st.dataframe(data)
+        st.plotly_chart(data)
+        st.write(data.describe())
+        st.write(data.corr())
+        st.write(data.info())
+        st.write(data.columns)
+        st.write(data.index)
+        st.write(data.shape)
+        st.write(data.size)
+        st.write(data.dtypes)
+        st.write(data.values)
+        st.write(data.head())
+        st.write(data.tail())
+        st.write(data.sample())
+        st.write(data.sort_values())
+        st.write(data.groupby())
+        st.write(data.pivot_table())
+        st.write(data.melt())
+        st.write(data.stack())
+        st.write(data.unstack())
+        st.write(data.transpose())
+        st.write(data.drop_duplicates())
+        st.write(data.fillna())
+        st.write(data.isna())
+        st.write(data.isnull())
+        st.write(data.notnull())
+        st.write(data.replace())
+        st.write(data.rename())
+        st.write(data.apply())
+        st.write(data.map())
+        st.write(data.filter())
+        st.write(data.where())
+        st.write(data.mask())
+        st.write(data.query())
+        st.write(data.eval())
+        st.write(data.select_dtypes())
+        st.write(data.clip())
+        st.write(data.abs())
+        st.write(data.round())
+        st.write(data.cumsum())
+        st.write(data.cumprod())
+        st.write(data.cummin())
+        st.write(data.cummax())
+        st.write(data.diff())
+        st.write(data.pct_change())
 
 
-from twitter_client import fetch_clients
-from executor.executor import TwitterExecutor
-from collector.collector import TwitterCollector
-from collector.trainer import AgentTrainer
-from strategy.strategy import TwitterStrategy
 
-# load environment variables
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-USER_ID = os.getenv("USER_ID", "")
+elif agent_token == "HuggingFaceSpaces":
+  hf_spaces.title(f"Dashboard for {agent_name}")
+  hf_spaces.dataframe(data)
+  hf_spaces.plotly_chart(data)
+  hf_spaces.markdown(f"""
+  - Data description: {data.describe()}
+  - Data correlation: {data.corr()}
+  - Data info: {data.info()}
+  - Data columns: {data.columns}
+  - Data index: {data.index}
+  - Data shape: {data.shape}
+  - Data size: {data.size}
+  - Data types: {data.dtypes}
+  - Data values: {data.values}
+  """)
+else:
+  raise ValueError("Invalid token")
 
+# Define a function to detect and prevent harmful content using Perspective API and Hatebase API
+def detect_and_prevent_harmful_content(tweet, agent):
+  # Get the tweet's text and author
+  tweet_text = tweet["text"]
+  tweet_author = tweet["author"]
 
-def async_command(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        return asyncio.run(f(*args, **kwargs))
+# Detect and prevent harmful content using Perspective API and Hatebase API
+  perspective_score = perspective_api_client.get_score(tweet_text)
+  hatebase_flag = hatebase_api_client.get_flag(tweet_text)
 
-    return wrapper
+  # If the tweet contains harmful content, take appropriate action
+  if perspective_score > 0.8 or hatebase_flag:
+# Log the harmful content detection
+    logging.warning(f"Detected harmful content in tweet: {tweet_text} from {tweet_author}")
 
+    # Delete the tweet using Tweepy or TwitterAPI
+    post_tweet(f"Sorry, this tweet has been deleted due to harmful content.", agent)
 
-@click.group()
-def cli():
-    pass
+    # Block the tweet's author using Tweepy or TwitterAPI
+    post_tweet(f"Sorry, this user has been blocked due to harmful content.", agent)
 
+    # Report the tweet's author to Twitter using Tweepy or TwitterAPI
+    post_tweet(f"Sorry, this user has been reported to Twitter due to harmful content.", agent)
 
-@cli.command()
-@click.option(
-    "--run-engine", default=False, is_flag=True, help="Run the engine."  # Same as above
-)
-@click.option(
-    "--test", default=False, is_flag=True, help="Test the engine."  # Same as above
-)
-@click.option(
-    "--ingest", default=False, is_flag=True, help="Ingest data into Weaviate."
-)
-@click.option(
-    "--train", default=False, is_flag=True, help="Train Model"
-)
-@async_command
-async def main(run_engine: bool, test: bool, ingest: bool, train: bool):
-    twitter_clients = fetch_clients()
-    weaviate_client = weaviate.Client("http://localhost:8080")
+  else:
+    # Log the harmless content detection
+    logging.info(f"Detected harmless content in tweet: {tweet_text} from {tweet_author}")
 
-    llm = OpenAI(temperature=0.9)
-    embeddings = OpenAIEmbeddings()
+# Define a function to update and improve data using PyTorch or TensorFlow
+def update_and_improve_data(data, agent):
+  # Get the agent's name and framework
+  agent_name = agent["name"]
+  agent_framework = agent["framework"]
 
-    # spawn collector, strategy, and executor for each client
-    agents = []
-    for twitter_client in twitter_clients:
-        client = twitter_client["client"]
-        agent_id = twitter_client["agent_id"]
-        agent_name = twitter_client["user_name"]
+  # Update and improve data using PyTorch or TensorFlow
+  if agent_framework == "PyTorch":
+    # Convert the data to a PyTorch tensor
+    data_tensor = torch.from_numpy(data)
 
-        vectorstore = Weaviate(weaviate_client, "Remilio", "content", embeddings)
+    # Apply some data transformations using PyTorch
+    data_tensor = torch.nn.functional.normalize(data_tensor)
+    data_tensor = torch.nn.functional.dropout(data_tensor)
+    data_tensor = torch.nn.functional.relu(data_tensor)
+    data_tensor = torch.nn.functional.softmax(data_tensor)
 
-        collector = TwitterCollector(agent_id, client, vectorstore, weaviate_client)
-        strategy = TwitterStrategy(llm, twitter_client, vectorstore)
-        executor = TwitterExecutor(agent_id, client)
+    # Return the updated data tensor
+    return data_tensor
 
-        if ingest:
-            await collector.ingest()
+  elif agent_framework == "TensorFlow":
+    # Convert the data to a TensorFlow tensor
+    data_tensor = tf.convert_to_tensor(data)
 
-        if train:
-            trainer = AgentTrainer(client, weaviate_client, OPENAI_API_KEY)
-            await trainer.run()
+    # Apply some data transformations using TensorFlow
+    data_tensor = tf.keras.layers.BatchNormalization()(data_tensor)
+    data_tensor = tf.keras.layers.Dropout()(data_tensor)
+    data_tensor = tf.keras.layers.Activation("relu")(data_tensor)
+    data_tensor = tf.keras.layers.Activation("softmax")(data_tensor)
 
-        agents.append((collector, strategy, executor, agent_name, agent_id))
+    # Return the updated data tensor
+    return data_tensor
 
-    # run
-    if run_engine:
-        await asyncio.gather(
-            *(
-                run(collector, strategy, executor, agent_name, agent_id, test)
-                for collector, strategy, executor, agent_name, agent_id in agents
-            )
-        )
+  else:
+    raise ValueError("Invalid framework")
 
+# Define a function to create a dialogue model using Rasa or Hugging Face
+def create_dialogue_model(agent):
+  # Get the agent's name and platform
+  agent_name = agent["name"]
+  agent_platform = agent["platform"]
 
-async def run(collector, strategy, executor, agent_name, agent_id, test):
-    print(f"\033[92m\033[1m\n*****Running {agent_name} Engine 🚒 *****\n\033[0m\033[0m")
+  # Create a dialogue model using Rasa or Hugging Face
+  if agent_platform == "Rasa":
+    # Train a dialogue model using Rasa
+    dialogue_model = rasa.train(domain="domain.yml", config="config.yml", stories="stories.md", nlu="nlu.md")
 
-    while True:
-        try:
-            # Step 1: Run Collector
-            print(
-                f"\033[92m\033[1m\n*****Running {agent_name} Collector 🔎 *****\n\033[0m\033[0m"
-            )
-            twitterstate = await collector.run()
+    # Save the dialogue model to a path
+    dialogue_model_path = f"{agent_name}_dialogue_model"
 
-            # Step 2: Pass timeline tweets to Strategy
-            print(
-                f"\033[92m\033[1m\n*****Running {agent_name} Strategy 🐲*****\n\033[0m\033[0m"
-            )
-            actions = strategy.run(twitterstate)
+    # Return the dialogue model path
+    return dialogue_model_path
 
-            # Step 4: Pass actions to Executor
-            print(
-                f"\033[92m\033[1m\n*****Running {agent_name} Executor🌠 *****\n\033[0m\033[0m"
-            )
-            if test:
-                pass
-            else:
-                executor.execute_actions(tweet_actions=actions)
+elif agent_platform == "HuggingFace":
+    # Load a dialogue model from Hugging Face
+    dialogue_model = pipeline("conversational", model="microsoft/DialoGPT-large")
 
-            # Sleep for an hour (3600 seconds) before the next iteration
-            print("Sleeping for an hour💤 💤💤")
-            await asyncio.sleep(3600)
-        except Exception as e:
-            print(f"Error in run: {e}")
+    # Save the dialogue model to a path
+    dialogue_model_path = f"{agent_name}_dialogue_model"
 
+    # Return the dialogue model path
+    return dialogue_model_path
 
-if __name__ == "__main__":
-    asyncio.run(main())
+  else:
+    raise ValueError("Invalid platform")
+
+# Define a function to generate a dialogue using Rasa or Hugging Face
+def generate_dialogue(tweet, agent):
+  # Get the tweet's text and author
+  tweet_text = tweet["text"]
+  tweet_author = tweet["author"]
+
+  # Get the agent's name and platform
+  agent_name = agent["name"]
+  agent_platform = agent["platform"]
+
+  # Generate a dialogue using Rasa or Hugging Face
+  if agent_platform == "Rasa":
+    # Load the dialogue model from a path
+    dialogue_model_path = f"{agent_name}_dialogue_model"
+    dialogue_model_agent = Agent.load(dialogue_model_path, interpreter=RasaNLUInterpreter("nlu_model_path"))
+
+    # Generate a dialogue using Rasa
+    dialogue_input = f"{tweet_author}: {tweet_text}"
+    dialogue_output = dialogue_model_agent.handle_text(dialogue_input)
+
+    # Return the dialogue output
+    return dialogue_output
+
+  elif agent_platform == "HuggingFace":
+    # Load the dialogue model from a path
+    dialogue_model_path = f"{agent_name}_dialogue_model"
+    dialogue_model = pipeline("conversational", model=dialogue_model_path)
+
+    # Generate a dialogue using Hugging Face
+    dialogue_input = f"{tweet_author}: {tweet_text}"
+    dialogue_output = dialogue_model(dialogue_input)
+
+    # Return the dialogue output
+    return dialogue_output
+
+  else:
+    raise ValueError("Invalid platform")
+
+# Define a function to tweet using Tweepy or TwitterAPI
+def tweet(tweet, agent):
+  # Get the tweet's text and author
+  tweet_text = tweet["text"]
+  tweet_author = tweet["author"]
+
+  # Get the agent's name and credentials
+  agent_name = agent["name"]
+  agent_credentials = agent["credentials"]
+
+  # Tweet using Tweepy or TwitterAPI
+if agent_credentials == "Tweepy":
+    # Tweet using Tweepy
+    twitter_client.tweet(tweet_text)
+
+  elif agent_credentials == "TwitterAPI":
+    # Tweet using TwitterAPI
+    twitter_api_client.tweet(tweet_text)
+
+  else:
+    raise ValueError("Invalid credentials")
+
+# Define a function to deploy and run the agents on Heroku or AWS
+def deploy_and_run(agents):
+  # Get the agents' names and services
+  agent_names = [agent["name"] for agent in agents]
+  agent_services = [agent["service"] for agent in agents]
+
+  # Deploy and run the agents on Heroku or AWS
+  for agent_name, agent_service in zip(agent_names, agent_services):
+    if agent_service == "Heroku":
+      # Deploy and run the agent on Heroku
+      os.system(f"heroku create {agent_name}")
+      os.system(f"git push heroku master")
+      os.system(f"heroku ps:scale worker=1")
+
+    elif agent_service == "AWS":
+      # Deploy and run the agent on AWS
+      os.system(f"aws configure")
+      os.system(f"aws s3 cp bot.py s3://{agent_name}/bot.py")
+      os.system(f"aws lambda create-function --function-name {agent_name} --runtime python3.8 --role arn:aws:iam::123456789012:role/lambda-role --handler bot.deploy_and_run --code S3Bucket={agent_name},S3Key=bot.py")
+
+    else:
+      raise ValueError("Invalid service")
+
+# Define a function to schedule or trigger the tweets using Cron or Lambda
+def schedule_or_trigger(agents):
+  # Get the agents' names and triggers
+  agent_names = [agent["name"] for agent in agents]
+  agent_triggers = [agent["trigger"] for agent in agents]
+
+  # Schedule or trigger the tweets using Cron or Lambda
+  for agent_name, agent_trigger in zip(agent_names, agent_triggers):
+    if agent_trigger == "Cron":
+      # Schedule the tweets using Cron
+      os.system(f"crontab -e")
+      os.system(f"echo '* * * * * python bot.py {agent_name}' >> crontab")
+
+    elif agent_trigger == "Lambda":
+      # Trigger the tweets using Lambda
+      os.system(f"aws lambda invoke --function-name {agent_name} --invocation-type Event --payload file://event.json response.json")
+
+    else:
+      raise ValueError("Invalid trigger")
